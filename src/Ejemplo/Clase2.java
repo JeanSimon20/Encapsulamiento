d0a788d1011490b7e8c2216826b3fd86b75966de
JeanSimon20 <jean.simon@vallegrande.edu.pe>
2022-04-28 17:13:38 -0500

    Ejemplo de encapsulamiento en JavaEE

---
+++ src/Ejemplo/Clase2.java
@@ -0,0 +1,13 @@
+package Ejemplo;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        Clase1 objeto1 = new Clase1();
+        
+        objeto1.setEdad(10);
+        System.out.println("La edad es: "+objeto1.getEdad());
+        
+        objeto1.setNombre("Jean");
+        System.out.println("El nombre es: "+objeto1.getNombre());
+    }
+}
